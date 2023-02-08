15a8d6149f469a1aa3f44727e2c5b2cf121218b7
FireNick44 | YannicA46 <yannic.studer44@gmail.com>
2023-02-08 16:29:54 +0100

    end of lesson 2

--- Uebung2.2/orders.php
+++ Uebung2.2/orders.php
@@ -1,1 +1,25 @@
 <?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "northwind";
+
+try {
+   $conn = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
+
+   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+   echo "Connected successfully <br> <br>";
+} catch (PDOException $e) {
+   echo "Connection failed: " . $e->getMessage() . "<br>";
+}
+
+
+//GET abfangen usw... SQL injection verhindern
+
+
+?>
+
+
+
+
+
